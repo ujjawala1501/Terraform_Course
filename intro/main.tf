e46bfa5668bffc3a9e46b5a6bc79ded449eef920
[Ujjawala Katakwar] <ujjawala1501@gmail.com>
2024-12-22 18:17:38 +0530

    explanation of code

--- intro/main.tf
+++ intro/main.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
+  required_providers {  #Providers chahiye (can be multiple providers )
+    aws = {    # AWS ka provider chaiye
       source = "hashicorp/aws"
-      version = "5.82.2"
+      version = "5.82.2"  # Iss version ka AWS ka provider chahiya ,terraform download karke laao.
     }
   }
 }
